01c7fa946054d02543c566d4bd48861e64f57314
ambsenyestudi <adrifre7@hotmail.com>
2022-03-14 22:59:31 +0100

    scafold

---
+++ test/domain/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace LeapYear.Domain.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void True()
+        {
+            Assert.True(true);
+        }
+    }
+}
